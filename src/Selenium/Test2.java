23b18000dbde6278f93a9413701038f69e383e8f
saikumar00198 <68212592+saikumar00198@users.noreply.github.com>
2020-08-18 07:44:43 +0530

    Update Test2.java

--- src/Selenium/Test2.java
+++ src/Selenium/Test2.java
@@ -1,10 +1,44 @@
 package Selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Test2 {
 
 	public static void main(String[] args) {
+		String brower = "Firefox";
+		WebDriver driver = null;
+
+		if (brower.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					"D:\\Workspace\\SeleniumPractice\\Executable Files\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		if (brower.equals("Firefox")) {
+			System.setProperty("webdriver.gecko.driver",
+					"D:\\Workspace\\SeleniumPractice\\Executable Files\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		}
 		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		System.out.println("Brower Open Successfully");
+       
+		driver.findElement(By.name("q")).sendKeys("Selenium Training");
+		driver.findElement(By.className("gNO89b")).click();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		driver.navigate().refresh();
+		driver.get("https://www.yahoo.com/");
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.quit();
 
 	}
 
 }
+
